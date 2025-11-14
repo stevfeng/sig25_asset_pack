1acdbb3381d28e633dcc85c6a7960322716de1d4
Ji Yuan Feng <168472921+stevfeng@users.noreply.github.com>
2025-11-13 17:21:19 -0800

    Fix path to isaac-sim.sh in launch script

--- launch_isaac_sim.sh
+++ launch_isaac_sim.sh
@@ -1,3 +1,3 @@
 source ~/isaac_sim_ros_workspaces/build_ws/humble/humble_ws/install/local_setup.bash
 source ~/isaac_sim_ros_workspaces/build_ws/humble/isaac_sim_ros_ws/install/local_setup.bash
-~/isaacsim/_build/linux-x86_64/release/isaac-sim.sh --reset-user
+~/isaacsim/isaac-sim.sh --reset-user
